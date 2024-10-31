6f45db8a5d8ab7f98af9410db80826f56132e2e4
airtonbjunior <airtonbjunior@gmail.com>
2024-10-31 10:52:52 -0300

    backend

--- index.js
+++ index.js
@@ -4,9 +4,36 @@ let app = express();
 const port = 3000;
 
 
-//app.METHOD(path, callback [, callback ...])
+/*
+    MÉTODO HTTP PARA RECUPERAR INFORMAÇÕES
+    /user -> retorna todos os usuários (ADM)
+    /user/:id -> retorna a informação do usuário id (ADM, próprio usuário)
+    /user/:id/registers -> retorna os registros do usuário id (ADM, próprio usuário)
+
+    MÉTODO HTTP PARA CRIAR RECURSOS
+    /user -> cria um usuário
+
+*/
+
+
+
+
+
+app.get("/user/:id1-:id2", (req, res) => {
+    console.log(req.params);
+});
+
+app.post("/user/:id1-:id2", (req, res) => {
+    res.send(req.params);
+});
+
 app.get("/teste", (req, res) => {
     res.send("Resposta da rota /teste");
+});
+
+
+app.post("/rotapost", (req, res) => {
+    res.send("Retorno da rota usando o método post");
 });
 
 
